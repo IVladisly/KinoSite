98a9a80408f6358cd2b5b1f666306907b0135640
IVladisly <86433171+IVladisly@users.noreply.github.com>
2022-01-17 00:06:25 +0200

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,13 @@
+new Swiper('.headerContainer', {
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+
+	pagination: {
+		el:'.swiper-pagination',
+		type: 'progressbar',
+	},
+
+
+});
