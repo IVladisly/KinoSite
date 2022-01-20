bc680895d1ea4152e3b4abaeb81392cbdced9738
IVladisly <86433171+IVladisly@users.noreply.github.com>
2022-01-20 23:41:09 +0200

    Add files via upload

--- logic.js
+++ logic.js
@@ -1,8 +1,4 @@
 new Swiper('.headerContainer', {
-	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev',
-	},
 
 	pagination: {
 		el:'.swiper-pagination',
@@ -13,13 +9,32 @@ new Swiper('.headerContainer', {
 	loop: true,
 
 
-	// autoplay: {
-	// 	delay: 3000, 
-	// 	disableOnInteraction: false, //Отключить автопрокрутку после взаимодействия
-	// },
+	autoplay: {
+		delay: 3000, 
+		disableOnInteraction: false, //Отключить автопрокрутку после взаимодействия
+	},
 
 	speed: 1100,
 
 
 
+});
+
+new Swiper ('.recomendedBlock__inner', {
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+	slidesPerView: 4,
+	slidesPerGroup: 4,
+	spaceBetween: 20,
+	speed: 600,
+
+	breakpoints: {
+		760: {
+			slidesPerView: 7,
+			slidesPerGroup: 6,
+		},
+
+	},
 });
